d4e7d698b5f5b99dc4e782a8942de3d9dc18c734
Bruno Santos <brunosantos@Brunos-MacBook-Pro.local>
2019-03-12 15:47:33 +0000

    add test app using Google Activity Recognition API

---
+++ app/src/main/java/com/example/brunosantos/testapp/MainActivity.java
@@ -0,0 +1,174 @@
+package com.example.brunosantos.testapp;
+
+import android.Manifest;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Environment;
+import android.preference.PreferenceManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.android.gms.location.ActivityRecognitionClient;
+import com.google.android.gms.location.DetectedActivity;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.ArrayList;
+public class MainActivity extends AppCompatActivity
+        implements SharedPreferences.OnSharedPreferenceChangeListener {
+    private Context mContext;
+    public static final String DETECTED_ACTIVITY = ".DETECTED_ACTIVITY";
+//Define an ActivityRecognitionClient//
+
+    private ActivityRecognitionClient mActivityRecognitionClient;
+    private ActivitiesAdapter mAdapter;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mContext = this;
+
+//Retrieve the ListView where we’ll display our activity data//
+        ListView detectedActivitiesListView = (ListView) findViewById(R.id.activities_listview);
+
+        ArrayList<DetectedActivity> detectedActivities = ActivityIntentService.detectedActivitiesFromJson(
+                PreferenceManager.getDefaultSharedPreferences(this).getString(
+                        DETECTED_ACTIVITY, ""));
+
+//Bind the adapter to the ListView//
+        mAdapter = new ActivitiesAdapter(this, detectedActivities);
+        detectedActivitiesListView.setAdapter(mAdapter);
+        mActivityRecognitionClient = new ActivityRecognitionClient(this);
+
+        Button walkingButton = (Button)findViewById(R.id.walking);
+        walkingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                writeFile("WALKING", "");
+            }
+        });
+        Button carButton = (Button)findViewById(R.id.car);
+        carButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                writeFile("CAR", "");
+            }
+        });
+        Button bicycleButton = (Button)findViewById(R.id.bicycle);
+        bicycleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                writeFile("BICYCLE", "");
+            }
+        });
+
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        PreferenceManager.getDefaultSharedPreferences(this)
+                .registerOnSharedPreferenceChangeListener(this);
+        updateDetectedActivitiesList();
+    }
+    @Override
+    protected void onPause() {
+        PreferenceManager.getDefaultSharedPreferences(this)
+                .unregisterOnSharedPreferenceChangeListener(this);
+        super.onPause();
+    }
+    public void requestUpdatesHandler(View view) {
+//Set the activity detection interval. I’m using 5 seconds//
+        Task<Void> task = mActivityRecognitionClient.requestActivityUpdates(
+                8000,
+                getActivityDetectionPendingIntent());
+        task.addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void result) {
+                updateDetectedActivitiesList();
+            }
+        });
+    }
+    //Get a PendingIntent//
+    private PendingIntent getActivityDetectionPendingIntent() {
+//Send the activity data to our DetectedActivitiesIntentService class//
+        Intent intent = new Intent(this, ActivityIntentService.class);
+        return PendingIntent.getService(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+    }
+    //Process the list of activities//
+    protected void updateDetectedActivitiesList() {
+        ArrayList<DetectedActivity> detectedActivities = ActivityIntentService.detectedActivitiesFromJson(
+                PreferenceManager.getDefaultSharedPreferences(mContext)
+                        .getString(DETECTED_ACTIVITY, ""));
+
+        mAdapter.updateActivities(detectedActivities);
+
+        for (DetectedActivity activity : detectedActivities) {
+            System.out.println("atividade -------- " + activity.getType() + "  -----  " + activity.getConfidence());
+            //write the activities to the file
+            writeFile(Integer.toString(activity.getType()), Integer.toString(activity.getConfidence()));
+        }
+    }
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
+        if (s.equals(DETECTED_ACTIVITY)) {
+            updateDetectedActivitiesList();
+        }
+    }
+
+    public void writeFile(String activityType, String activityConfidence) {
+        if(isExternalStorageWritable()) {
+            checkPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            File externalStorageDir = Environment.getExternalStorageDirectory();
+            File myFile = new File(externalStorageDir, "test.txt");
+
+            try {
+                String toWrite = activityType + ":" + activityConfidence + "\n\r";
+                long fileLength = myFile.length();
+                RandomAccessFile raf = new RandomAccessFile(myFile, "rw");
+                raf.seek(fileLength);
+                raf.writeBytes(toWrite);
+                raf.close();
+
+                Toast.makeText(this, "Saved line", Toast.LENGTH_SHORT).show();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        else {
+            Toast.makeText(this, "Permission denied!", Toast.LENGTH_SHORT).show();       }
+    }
+
+    public boolean isExternalStorageWritable() {
+        System.out.println(Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState()));
+        return Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState());
+    }
+
+    public void checkPermission(String permission){
+        String[] PERMISSIONS_STORAGE = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        int check = ContextCompat.checkSelfPermission(this, permission);
+        System.out.println(check + "   111111111111111");
+
+        if (check != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                    this,
+                    PERMISSIONS_STORAGE,
+                    1);
+        }
+     }
+
+}
